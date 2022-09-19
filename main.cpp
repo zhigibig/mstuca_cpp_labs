239ba772e0984b4d54e38516bfe3b90ad354c815
mr_libertariat <95529471+zhigibig@users.noreply.github.com>
2022-09-19 09:29:55 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
-#include <iostream>
-#include <cmath>
-#include <iomanip>
+#include <iostream> 
+#include <cmath> 
+#include <iomanip> 
 
+bool enterance_checking(long double var1, long double var2) {
+    if (var1 <= 1 && var2 <= 1) {
+        return true;
+    } else if ( (var1 <= 2) && (var2 <= 1.5) ) {
+        return true;
+    } else {
+        return false;
+    };
+};
 
 int main() {
     using namespace std;
@@ -11,22 +20,26 @@ int main() {
         (a + b) * e^      + lg b^                      -3
     z = ---------------------------  + 2,85 * |y| * 10^
            a + 1 - sin( x^(7/2))
-           
-    z = result
-    a = A
-    b = B
-    x = var1
-    y = var2
     */
 
-    const float A = 2.4, B = 3.81;
-    long double var1, var2; 
+    const float a = 2.4, b = 3.81;
+    long double x, y; 
     long double result;
+    string enterance_check_res;
 
-    for (var1 = 0; var1 < 3; var1++) {
-        for (var2 = 0; var2 < 3; var2++) {
-            result = ((A + B) * exp(2.3 * var2) + log(pow(B, 3)) / (A + 1) - sin(pow(var1, 3.5))) + 0.00285 * abs(var2);
-            std::cout << setw(3) << var1 << setw(3) << var2 << setw(15) << result << std::endl;
+    cout << setw(6) << "X" << setw(6) << "Y" << setw(10) << "Result" << setw(13) << "Graph pos" << "\n" << endl;
+
+    for (x = 0; x <= 2; x += 0.25) {
+        for (y = 0; y <= 2; y += 0.25) {
+            result = ((a + b) * exp(2.3 * y) + log(pow(b, 3)) / (a + 1) - sin(pow(x, 3.5))) + 0.00285 * abs(y);
+
+            if ( enterance_checking(x, y) == 1 ) {
+                enterance_check_res = "true";
+            } else {
+                enterance_check_res = "false";
+            };
+
+            cout << setw(6) << x << setw(6) << y << setw(10) << result << setw(13) << enterance_check_res << endl;
         };
     };
 
