24471918d876697f1a01099fcdc2be0859d8be74
mr_libertariat <95529471+zhigibig@users.noreply.github.com>
2022-09-19 19:24:40 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 #include <iomanip> 
 
 bool enterance_checking(long double var1, long double var2) {
+    /*
+    Функция для проверки вхождения координат в график
+    */
     if (var1 < 0 || var2 < 0) {
         return false;
     } else if (var1 <= 1 && var2 <= 1) {
@@ -22,7 +25,14 @@ int main() {
         (a + b) * e^2,3*y     + lg b^3               
     z = ---------------------------  + 2,85 * |y| * 10^-3
            a + 1 - sin( x^(7/2))
-           
+
+    
+    Обозначения в программе 
+    z = result 
+    a = a
+    b = b
+    x = x
+    y = y
     */
 
     const float a = 2.4, b = 3.81;
@@ -32,8 +42,8 @@ int main() {
 
     cout << setw(6) << "X" << setw(6) << "Y" << setw(10) << "Result" << setw(13) << "Graph ent" << "\n" << endl;
 
-    for (x = -3; x <= 3; x += 0.5) {
-        for (y = 0-3; y <= 3; y += 0.5) {
+    for (x = -1; x <= 2.5; x += 0.5) {
+        for (y = -1; y <= 2.5; y += 0.5) {
             result = ((a + b) * exp(2.3 * y) + log(pow(b, 3)) / (a + 1) - sin(pow(x, 3.5))) + 0.00285 * abs(y);
 
             if ( enterance_checking(x, y) == 1 ) {
