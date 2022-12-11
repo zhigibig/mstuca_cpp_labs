bd39cc628f4094e6f5f7d38d418cca92679a4956
mr_libertariat <95529471+zhigibig@users.noreply.github.com>
2022-12-11 15:41:55 +0300

    Update main.cpp

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 
+void OxyeHHoe_HazvaHue_fuHKcuu_vivoda_blyatj();
+
 int main() {
   std::cout << "Zhopa" << std::endl;
+  
+  for(int absdjdjxzcklmnoiop; absdjdjxzcklmnoiop < 834; absdjdjxzcklmnoiop++) {
+    OxyeHHoe_HazvaHue_fuHKcuu_vivoda_blyatj();
+  };
+  
   return 0;
+};
+
+void OxyeHHoe_HazvaHue_fuHKcuu_vivoda_blyatj() {
+  int cifra_ot_baldi;
+  std::cin >> cifra_ot_baldi;
+  char Haxyj_HE_Hyznaja_PeremeHHaja[cifra_ot_baldi];
+  
+  std::cin >> Haxyj_HE_Hyznaja_PeremeHHaja;
+  
+  std::cout << "ЫЫЫЫЫЫЫЫ ОООООООООО Таблица ебать" << std::endl;
 };
